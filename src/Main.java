7a97081e0d6ed76ea89529c3aeef4b2f97ef9694
Mashanov Artemyi <ms.mashanov@mail.ru>
2023-12-15 17:46:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int k = 20; // для одного бонуса
+        int x = 12312312; //стоимость билета
+        int z = (x/k); // переменная для хранения
+        System.out.println(z);
+    }
+}
