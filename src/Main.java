fabeea17c12fb1a77512acf5aa9ac9832aa8fecb
Mashanov Artemyi <ms.mashanov@mail.ru>
2023-12-19 23:29:26 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int numberForOneBonusMile = 20; // для одного бонуса
         int ticketPrice = 12312312; //стоимость билета
-        int numberOfBonusMiles = (ticketPrice/numberForOneBonusMile); // переменная для хранения
+        int numberOfBonusMiles = (ticketPrice / numberForOneBonusMile); // переменная для хранения
         System.out.println(numberOfBonusMiles);
     }
 }
